1b4aa94f0e30c0d2461642e89c2522db1329fb39
Ftejero21 <fernandotejero566@gmail.com>
2023-05-29 18:49:26 +0200

    modificando sentencia sql

--- src/main/java/com/example/empresaback/Repository/EmailRepository.java
+++ src/main/java/com/example/empresaback/Repository/EmailRepository.java
@@ -11,6 +11,6 @@ public interface EmailRepository extends JpaRepository<Email,Long> {
 
     public List<Email> findByEmpresarioIdOrderByIdDesc(Long empresarioId);
 
-    @Query(value = "SELECT DISTINCT FROM emails WHERE email = :email order by id desc" , nativeQuery = true)
+    @Query(value = "SELECT DISTINCT email FROM emails WHERE email = :email", nativeQuery = true)
     public List<Email> findByEmail(@Param("email") String email);
 }
